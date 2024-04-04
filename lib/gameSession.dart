caf9e448704f14a9d6f743dd408c21384fc28d05
M.Mika <Abovepolaris110@gmail.com>
2024-04-04 11:44:50 +0900

    created gameSession.dart to modulize with metronome.

---
+++ lib/gameSession.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'dart:math' as math;
+import 'dart:io';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame/palette.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+import 'package:metronome/metronome.dart';
